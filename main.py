48500f3910d0a977318cfc58348295e7b9b24f82
ARNAB MUKHERJEE <119810904+arnabm-94@users.noreply.github.com>
2022-12-05 19:12:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #Exercise 1: Make a Calculator
-a = int(input("Enter your first number"))
-b = int(input("Enter your second number"))
-c = print("your choices are - 1. + 2. - 3. * 4. /  5. %")
-d = input("Enter the operator which you want to perform with those two numbers")
-if (d == "+"):
-    print(f"The sum of {a} and {b} is", a+b)
-elif d == "-":
-    print(f"The substraction of {a} and {b} is", a-b)
-elif d == "*":
-    print(f"The product of {a} and {b} is", a*b)
-elif d == "/":
-    print(f"The division of {a} and {b} is", a/b)
-elif d == "%":
-    print(f"The modulus of {a} and {b} is", a%b)
-9887
+#a = int(input("Enter your first number"))
+#b = int(input("Enter your second number"))
+#c = print("your choices are - 1. + 2. - 3. * 4. /  5. %")
+#d = input("Enter the operator which you want to perform with those two numbers")
+#if (d == "+"):
+#    print(f"The sum of {a} and {b} is", a+b)
+#elif d == "-":
+#    print(f"The substraction of {a} and {b} is", a-b)
+#elif d == "*":
+#   print(f"The product of {a} and {b} is", a*b)
+#elif d == "/":
+#    print(f"The division of {a} and {b} is", a/b)
+#elif d == "%":
+#    print(f"The modulus of {a} and {b} is", a%b)
+
+a = 50
+b = 34
+print("The value of", a, "+", 3, "is: ", a+b)
+print("The value of", a, "-", 3, "is: ", a-b)
+print("The value of", a, "/", 3, "is: ", a/b)
+print("The value of", a, "*", 3, "is: ", a*b)
